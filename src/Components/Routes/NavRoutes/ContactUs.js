7ddc54d69eaf363246e6f2110a1089311fba9c4f
rishii64 <saptarsismmitra@gmail.com>
2024-02-11 14:43:01 +0530

    updated

--- src/Components/Routes/NavRoutes/ContactUs.js
+++ src/Components/Routes/NavRoutes/ContactUs.js
@@ -2,6 +2,56 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className='contactUs'>
+      <div className="container">
+        <div className='sectionLeft'>
+          <ul>
+            <li>
+              Address
+              <p>Mada Center 8th floor, 379 Hudson</p>
+              <p>St. New York, NY 10018 US</p>
+            </li>
+            <li>
+              Let's Talk
+              <p>+91 9876543210</p>
+            </li>
+            <li>
+              General Support
+              <p>contact@indiatourism.com</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className='sectionRight'>
+          <form autocomplete="off">
+            <h3 className='header'>Send Us A Message</h3> <br />
+            <div className='form-item'>
+              <input type="text" name='name' required /> <br />
+              <label>Name</label> <br />
+            </div>
+
+            <div className='form-item'>
+              <input type="text" name='mail' required />  <br />
+              <label>E-mail</label> <br />
+            </div>
+
+            <div className='form-item'>
+              <input type="number" name='pass' required />  <br />
+              <label>Phone</label> <br />
+            </div>
+            <div className='form-item'>
+              <label>Write us a message</label> <br/>
+              <textarea rows="6" cols="40"></textarea>
+            </div> <br/>
+
+            <button className='btnSend'>
+              <span class="shadow"></span> <span class="edge"></span>
+              <span class="front text">Send</span>
+            </button> <br />
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
--- src/Components/Routes/NavRoutes/PlacesToVisit.js
+++ src/Components/Routes/NavRoutes/PlacesToVisit.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function PlacesToVisit() {
+  const [states, setStates] = useState([])
+  useEffect(() => {
+    try {
+      axios.get('http://localhost:4343/placesToVisit')
+        .then(res => {
+          setStates(res.data)
+        })
+    }
+    catch (err) {
+      console.error('Error:', err);
+    }
+  }, [])
+  // console.log(states);
   return (
-    <div>PlacesToVisit</div>
+    <>
+      <img className='placesToVisitImg' src='https://www.theindiatourism.com/images/places-to-visit-india.jpg' alt='' />
+      <div className="placesToVisit">
+        <h1>Top Tourist Destination India</h1>  <hr /> <br />
+        <p><b>Top Tourist Destination India</b> is the home to numerous attractions, amenities, activities and beauties. It is not hard to believe that a land as diverse as India has a long list of travel destinations within its border. Starting from the valleys of <b>Kashmir</b> to the tri-colored sea of <b>Kanchipuram</b>, you will find numerous tourist attractions, which would suit your taste. If you were visiting India in winter, <b>Rajasthan</b>, <b>Agra</b>, <b>Maharashtra</b>, <b>Tamil Nadu</b>, <b>Kerala</b>, <b>Chandigarh</b>, <b>Karnataka</b>, Goa and other places would be in the top choice. If you choose to visit India in the monsoon, <b>Uttarakhand</b>, <b>Munnar</b>, <b>Mussorie</b>, <b>Cherrapunji</b> and Coorg are some of the places to choose. </p>
+      </div>
+
+      <div className="visitStates">
+        {
+          states.map((item, index) => {
+            return (
+              <div className='states' key={index}>
+                <h4 className='stateTitle'>{item.title}</h4>
+                <img src={item.poster} alt='' />
+                <div className="div">
+                  {
+                    item.placeList.map((item, index) => {
+                      return (
+                        <ul key={index}>
+                          <li>{item}</li>
+                        </ul>
+                      )
+                    })
+                  }
+                </div>
+                <p>View All</p>
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </>
   )
 }
